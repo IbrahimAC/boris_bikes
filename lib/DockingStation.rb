b26d7863341a29a1216d0755650e132531f21f23
Sabrina <sabrinalord@msn.com>
2021-11-17 15:22:01 +0000

    passed r spec added working method

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,5 +1,13 @@
+
 class DockingStation
+
+
+
     def release_bike
         
+    end
+
+    def working?(bicycle)
+    
     end
 end
---
+++ spec/bike_spec.rb
@@ -0,0 +1,9 @@
+require 'bike'
+
+describe Bike do 
+   bike = Bike.new
+   it "expects Bike instances to respond_to the method working?" do
+     expect(bike).to respond_to(:working?)
+   end
+end 
+
