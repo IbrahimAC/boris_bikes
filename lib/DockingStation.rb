58902c41c4253bd768a2b0372a9315150c368583
Sabrina <sabrinalord@msn.com>
2021-11-17 17:19:41 +0000

    checking that the user can dock a bike

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,10 +1,25 @@
 class DockingStation
+
+  # def initialize(bike)
+  #    @bike = bike
+
+  # end 
+
+
     def release_bike #release_bike returns a new instance of the Bike class.
       Bike.new
+      @bike = bike 
     end
+ 
 end
 
 class Bike
+
+  def bike_docking
+
+    station_one = DockingStation.new(@bike)
+  end
+
     def working?
       true
     end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -15,5 +15,16 @@ describe DockingStation do
     bike = docking_station.release_bike
     expect(bike.working?).to eq true # fails with "expected true, got false (using ==)"
   end
+   
+  it "lets you dock the bike" do
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
 
+    expect(bike.bike_docking).to be_instance_of(DockingStation) # same as obj.should be_instance_of(type)
+   
+    # expects docking station to be an instance of docking station. 
+    # In the docking_the_bike method we'll create a new instance of docking station
+    # with the bike instance of an atrribute of it 
+
+  end
 end
