27fbb9dbb250a1a44d6c1dfa430a882e74e2ceba
IbrahimAC <ibrahimchowdhury.notwork@gmail.com>
2021-11-17 16:06:09 +0000

    making docking_station.release_bikes create a bike(an instance of our Bike class

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,13 +1,13 @@
-
 class DockingStation
-
-
-
-    def release_bike
-        
-    end
-
-    def working?(bicycle)
-    
+    def release_bike #release_bike returns a new instance of the Bike class.
+        bike = Bike.new
     end
 end
+
+class Bike
+    def working?
+      true
+    end
+end 
+docking_station = DockingStation.new
+puts docking_station.release_bike
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -4,6 +4,10 @@ describe DockingStation do
     docking_station = DockingStation.new
     expect(docking_station).to respond_to(:release_bike) 
   end
-  
+  it "expects release_bike to 'get a bike' meaning create a instance of one" do
+    docking_station = DockingStation.new
+    
+    expect(docking_station.release_bike).to be_instance_of(Bike) # same as obj.should be_instance_of(type)
+  end
 
 end
